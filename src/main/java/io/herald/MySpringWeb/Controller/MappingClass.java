4d3e48fba9a8e721d6321d43f7b649afbf1bd48a
Adarsh-shres <ansbm3299@gmail.com>
2026-07-13 11:55:20 +0545

    Grp1July13Commit

--- src/main/java/io/herald/MySpringWeb/Controller/MappingClass.java
+++ src/main/java/io/herald/MySpringWeb/Controller/MappingClass.java
@@ -1,7 +1,9 @@
 package io.herald.MySpringWeb.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller //handles Http Requests: Get, Post, etc
 public class MappingClass {
@@ -13,11 +15,35 @@ public class MappingClass {
     @GetMapping("/nextPage")
     public  String openNextPage()
     {
+
         return "nextPage.html";
     }
     @GetMapping("/login")
     public String openLoginPage()
     {
         return "loginPage.html";
+    }
+
+
+
+    @PostMapping("/login")
+    public String loginPost(HttpServletRequest request)
+    {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+            System.out.println(username);
+            System.out.println(password);
+
+            if(username.equals("admin") && password.equals("admin"))
+            {
+                return "home.html";
+            }
+                return "loginPage.html";
+
+
+        //When a form data does post request, HttpServletRequest obtains
+            //those data as parameters
+
     }
 }
---
+++ src/main/java/io/herald/MySpringWeb/Repository/UserRepository.java
@@ -0,0 +1,16 @@
+package io.herald.MySpringWeb.Repository;
+
+import io.herald.MySpringWeb.Model.UserTable;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+//Repository -> Uses JPA and Hibernate to connect to our
+//database and tables
+public interface UserRepository extends JpaRepository<UserTable, Integer> {
+
+        //Custom Query
+        //join query
+
+}
