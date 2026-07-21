93a255bcd3f93ff5b8d3d4434ee36c79c9126e3f
Adarsh-shres <ansbm3299@gmail.com>
2026-07-21 11:56:01 +0545

    Grp1July21Commit

--- src/main/java/io/herald/MySpringWeb/Controller/MappingClass.java
+++ src/main/java/io/herald/MySpringWeb/Controller/MappingClass.java
@@ -3,6 +3,7 @@ package io.herald.MySpringWeb.Controller;
 import io.herald.MySpringWeb.Model.UserTable;
 import io.herald.MySpringWeb.Repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -53,13 +54,32 @@ public class MappingClass {
                 List<UserTable> totalUsers = uRepo.findAll();
                 m.addAttribute("totalUsers", totalUsers);
 
+                HttpSession session = request.getSession();
+                session.setAttribute("username",username);
+
+
                 return "home.html";
             }
-                return "loginPage.html";
 
+
+        return "loginPage.html";
 
         //When a form data does post request, HttpServletRequest obtains
             //those data as parameters
 
+    }
+
+
+    @GetMapping("/home")
+    public String homeGet(Model m){
+    m.addAttribute("totalUsers", uRepo.findAll());
+    return "home";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request)
+    {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        return "loginPage.html";
     }
 }
